8cfd1c5f90f27febc67d5f9264123284dcf65ee2
hebustrolle <ismael.hadj13@gmail.com>
2017-12-04 14:33:46 +0100

    first commit

---
+++ cache/production/sql_7b3fb549ad06b2e8e25151bd29d2849c.php
@@ -0,0 +1,5 @@
+<?php exit; ?>
+1511373515
+SELECT * FROM php_bbstyles s WHERE s.style_id = 0
+6
+a:0:{}
---
+++ cache/production/sql_c7149b85a28cb5d60355d5be84466e67.php
@@ -0,0 +1,5 @@
+<?php exit; ?>
+1511370515
+SELECT forum_id FROM php_bbforums WHERE forum_options & 2 <> 0 LIMIT 1
+6
+a:0:{}
